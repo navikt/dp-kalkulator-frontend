ca353cd1c8d2b7292482009ab929bf7cee2de458
Amund Bergland Kvalsvik <Amund.Bergland.Kvalsvik@nav.no>
2019-06-27 14:22:07 +0200

    Dynamically generate lists from JSON. Fix naming

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            inntekt: 0,
-            maaneder: 0,
-            result: false,
-            allMonths: null
+            totalIncome: null,
+            employerSummaries: null,
+            monthsIncomeInformation: null,
+            doesPersonQualify: false
         };
     }
 
@@ -17,14 +17,13 @@ class App extends React.Component {
         return (
             <div className="App">
                 <h1>Vår info </h1>
-                <div>Din totale inntekt: {this.state.totalInntekt} </div>
+                {this.state.totalIncome === null ? <div/> : <TotalInntekt totalIncome={this.state.totalIncome}/>}
                 <div>Laster fortsatt: {this.state.loading ? "yes" : "no"} </div>
-                <div>Din inntekt siste {this.props.month} måneder: {this.state.inntekt} </div>
-                <div>Ansettelse siste {this.props.month} måneder: {this.state.maaneder} </div>
-                <div>Kvalifisert for dagpenger: {this.state.result} </div>
                 <div>Hvis noe av oppgitt data her er feil, kontakt oss på nav.no</div>
-                <div>Personnummeret ditt er: {this.state.pn} </div>
-                {this.state.allMonths === null ? <div/> : <AllMonths months={this.state.allMonths}/>}
+                <div>Personnummeret ditt er: {this.state.personnummer} </div>
+                <div>Kvalifisert for dagpenger: {this.state.doesPersonQualify ? "ja" : "nei"} </div>
+                {this.state.employerSummaries === null ? <div/> : <EmployerList employerSummaries={this.state.employerSummaries}/>}
+                {this.state.monthsIncomeInformation === null ? <div/> : <AllMonths monthsIncomeInformation={this.state.monthsIncomeInformation}/>}
 
             </div>
         );
@@ -32,25 +31,18 @@ class App extends React.Component {
 
 
     componentDidMount() {
-        const result = this.state.result ? "ja" : "nei";
-        const pn = getQueryVariable("pn", window.location.pathname);
+        const personnummer = getQueryVariable("personnummer", window.location.pathname);
         this.setState({
-            inntekt: 490300,
-            month: 12,
-            maaneder: 8,
-            pn: pn,
-            result: result,
+            personnummer: personnummer,
             loading: true
         });
-        const json = '{"maaneder":[{"dato":"2019-07","arbeidsgivere":[{"navn":"NAV","organisasjonsID":"144132","inntekt":"58.32"}]}]}';
+        const json = '{"totalIncome":"1858201","employerSummaries":[{"name":"NAV","orgID":"144132","income":"1858201","startMonth":"2019-07","endMonth":"2019-07"}],"monthsIncomeInformation":[{"month":"2019-07","employers":[{"name":"NAV","orgID":"144132","income":"58.32"}]}]}';
         const obj = JSON.parse(json);
         this.setState({
-            allMonths: obj.maaneder
+            monthsIncomeInformation: obj.monthsIncomeInformation,
+            totalIncome: obj.totalIncome,
+            employerSummaries: obj.employerSummaries
         });
-        console.log("AllMonthsComing");
-        console.log(json);
-        console.log(obj);
-        console.log(this.state.allMonths);
         /*fetch("foo.bar/", {
             method: "POST",
             headers: {
@@ -58,21 +50,21 @@ class App extends React.Component {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify({
-                pn
+                personnummer
             })
         })
-            .then(result => {
-                if (result.ok) {
-                    return result.json();
+            .then(doesPersonQualify => {
+                if (doesPersonQualify.ok) {
+                    return doesPersonQualify.json();
                 } else {
-                    throw Error(result.statusText);
+                    throw Error(doesPersonQualify.statusText);
                 }
             })
             .then(json => {
 
                 this.setState({
                     isLoaded: true,
-                    totalInntekt: json.totalInntekt
+                    totalIncome: json.totalIncome
             })
                 ;
             })
@@ -80,31 +72,60 @@ class App extends React.Component {
     }
 }
 
+function EmployerList(props) {
+    return (
+        <ul>
+            {props.employerSummaries.map(employer => <EmployerSummary employer={employer}/>)}
+        </ul>
+    );
+}
+
+function TotalInntekt(props) {
+    return (
+        <div>
+            Din totale inntekt de siste 36 måneder: {props.totalIncome}
+        </div>
+    );
+}
+
+function EmployerSummary(props) {
+    return (
+        <li>
+            <div>Arbeidsgiver: {props.employer.name}</div>
+            <div>Organisasjonsnummer: {props.employer.orgID}</div>
+            <div>Total inntekt: {props.employer.income}</div>
+            <div>Fra: {props.employer.startMonth}</div>
+            <div>Til: {props.employer.endMonth}</div>
+        </li>
+    )
+}
 
 function EmployersMonth(props) {
     return (
-        <div>
-            <div>Måned: {props.month.dato} </div>
-            {props.month.arbeidsgivere.map(arbeidsgiver => <Employer arbeidsgiver={arbeidsgiver}/>)}
-        </div>
+        <li>
+            <div>Måned: {props.month.month} </div>
+            <ul>
+                {props.month.employers.map(arbeidsgiver => <Employer employer={arbeidsgiver}/>)}
+            </ul>
+        </li>
     );
 }
 
 function Employer(props) {
     return (
-        <div>
-            <div>Arbeidsgiver: {props.arbeidsgiver.navn}</div>
-            <div>OrganisasjonsID: {props.arbeidsgiver.organisasjonsID}</div>
-            <div>Inntekt: {props.arbeidsgiver.inntekt}</div>
-        </div>
+        <li>
+            <div>Arbeidsgiver: {props.employer.name}</div>
+            <div>OrganisasjonsID: {props.employer.orgID}</div>
+            <div>Inntekt: {props.employer.income}</div>
+        </li>
     )
 }
 
 function AllMonths(props) {
     return (
-        <div>
-            {props.months.map(month => <EmployersMonth month={month}/>)}
-        </div>
+        <ul>
+            {props.monthsIncomeInformation.map(month => <EmployersMonth month={month}/>)}
+        </ul>
     );
 }
 
