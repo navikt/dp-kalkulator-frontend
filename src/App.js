24b0c82767ae39ebd2e66629ed58d5165f815062
sherveep <sherveep@stud.ntnu.no>
2019-07-10 12:15:07 +0200

    lagt inn tekster

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import PanelBase from 'nav-frontend-paneler';
 import { Sidetittel, Normaltekst, Innholdstittel } from 'nav-frontend-typografi';
 import NavFrontendSpinner from 'nav-frontend-spinner';
 import AlertStripe from 'nav-frontend-alertstriper';
-import { HjelpetekstAuto } from 'nav-frontend-hjelpetekst';
 
 class App extends React.Component {
   constructor(props) {
@@ -21,6 +20,9 @@ class App extends React.Component {
   render() {
     return (
         <div className="App">
+
+            <img src="https://www.nav.no/_public/beta.nav.no/images/logo.png?_ts=1512923c9b0" alt={"NAV-logo-rød-trans-bg-200.png"}/>
+
             <Sidetittel>Din inntekt {this.state.loading ? <NavFrontendSpinner/> : <br/>} </Sidetittel>
             <p>
             <b>Her vises opplysninger om dine inntekter. NAV bruker blant annet disse opplysningene for å vurdere om du har rett til dagpenger, hvor lenge og hvor mye du kan få i dagpenger.</b>
@@ -28,10 +30,7 @@ class App extends React.Component {
             {this.state.totalIncome12 === null ? <br/> : <TotalInntekt12 totalIncome12={this.state.totalIncome12}/>}
             <Normaltekst>Din arbeidsgiver og andre som utbetaler inntekter til deg rapporterer disse opplysningene til a-ordningen minst én gang i måneden. Oppdager du feil? Ta kontakt med de som har rapportert opplysningene.</Normaltekst>
             </p>
-            <p>
 
-                <HelpTextForRejection> </HelpTextForRejection>
-            </p>
             <PanelBase border>
                 {this.state.employerSummaries === null ? <br/> :
                     <EmployerList
@@ -40,6 +39,7 @@ class App extends React.Component {
                     <AllYears
                         monthsIncomeInformation={this.state.monthsIncomeInformation}/>}
             </PanelBase>
+            <QualifiedMessage doesPersonQualify={this.state.doesPersonQualify}/>
         </div>
     );
   }
@@ -79,30 +79,49 @@ class App extends React.Component {
 function QualifiedMessage(props) {
     return (
         <AlertStripe type={props.doesPersonQualify ? "suksess" : "feil"}>
-            {props.doesPersonQualify ? <PositivResponse> </PositivResponse>  : <PositivResponse> </PositivResponse>}
+            {props.doesPersonQualify ? <PositivResponse/>   : <NegativeResponse/>}
         </AlertStripe>
     );
 }
-
-function PositivResponse(props) {
+function PositivResponse() {
     return (
         <div>
         <Normaltekst>
-            <p>
+            <h5> Du kan ha tjent nok til å ha rett på dagpenger</h5>
+
             A-ordningen viser at du har hatt inntekter over 1,5 ganger folketrygdens grunnbeløp (1,5 G) de siste 12 månedene eller 3 G de siste 36 månedene, og kan derfor ha tjent nok til å ha rett på dagpenger.
-            </p>
             <p>
-                Du kan likevel avslag på søknaden på grunn av for lav inntekt
-                <HjelpetekstAuto tittel={""} >
-                    Du har mottatt dagpenger i løpet av de siste 36 månedene, og inntekter som vises her har allerede blitt brukt opp i en tidligere dagpengeperiode. Du kan i mange tilfeller ha rett på dagpenger selv om du har mottatt dagpenger de siste 36 månedene. (OBS: regner med at hele denne kun vises for de som søker vanlig, og ikke gjenopptak?)
-
-                    · Du får innvilget dagpenger fra en dato fram i tid. Da kan nyere inntekter bli tatt med i vurderingen av om du har rett på dagpenger. Dette kan i noen tilfeller kan føre til at du ikke har tjent nok.
-
-                    · Opplysningene i a-ordningen er feil, for eksempel fordi arbeidsgiveren din har rapportert for høy inntekt for deg en måned.
-
-            </HjelpetekstAuto>
+                <b >Du kan likevel avslag på søknaden på grunn av for lav inntekt hvis:</b>
+                <ul>
+                <li>Du har mottatt dagpenger i løpet av de siste 36 månedene, og inntekter som vises her har allerede blitt brukt opp i en tidligere dagpengeperiode. Du kan i mange tilfeller ha rett på dagpenger selv om du har mottatt dagpenger de siste 36 månedene. </li>
+                <li>Du får innvilget dagpenger fra en dato fram i tid. Da kan nyere inntekter bli tatt med i vurderingen av om du har rett på dagpenger. Dette kan i noen tilfeller kan føre til at du ikke har tjent nok.</li>
+                <li>Opplysningene i a-ordningen er feil, for eksempel fordi arbeidsgiveren din har rapportert for høy inntekt for deg en måned. </li>
+                </ul>
             </p>
+            <b>Vi anbefaler at du sender en søknad om dagpenger selv hvis du er usikker på om du har tjent nok, så kan en saksbehandler vurdere dette.</b>
         </Normaltekst>
+        </div>
+    );
+}
+
+function NegativeResponse() {
+    return (
+        <div>
+            <Normaltekst>
+                <h4> Du kan ha tjent for lite til å ha rett på dagpenger</h4>
+                    A-ordningen viser at du har hatt inntekter under 1,5 ganger folketrygdens grunnbeløp (1,5 G) de siste 12 månedene eller 3 G de siste 36 månedene, og kan derfor ha tjent for lite til å ha rett på dagpenger.
+                <p>
+
+                    <b>Følgende hendelser kan likevel gjøre at du har tjent nok til å ha rett på dagpenger:</b>
+                    <ul>
+                    <li>Du har hatt verneplikt, og kan likevel ha rett på dagpenger </li>
+                    <li>Du får innvilget dagpenger fra en dato fram i tid. Da kan nyere inntekter bli tatt med i vurderingen av om du har rett på dagpenger og dette kan føre til at du har tjent nok </li>
+                    <li>Du har hatt arbeid i et annet EØS-land. Opplysninger om lønn fra andre EØS-land registreres ikke i a-ordningen. </li>
+                    <li>Opplysningene i a-ordningen er feil, for eksempel fordi arbeidsgiveren din har rapportert for høy inntekt for deg en måned. </li>
+                    </ul>
+                </p>
+                <b>Vi anbefaler at du sender en søknad om dagpenger selv hvis du er usikker på om du har tjent nok, så kan en saksbehandler vurdere dette.</b>
+            </Normaltekst>
         </div>
     );
 }
@@ -112,12 +131,12 @@ function TotalInntekt(props) {
     return (
         <Normaltekst>
 
-                <li>
+                    <li>
                     Mine inntekter de siste 36 månedene: {props.totalIncome.toFixed(2)} kr
-                </li>
-                <li>
+                    </li>
+                    <li>
                     Min gjennomsnittlige inntekt de siste 36 månedene: {(props.totalIncome / 36).toFixed(2)}kr.
-                </li>
+                    </li>
 
             </Normaltekst>
     );
@@ -152,14 +171,7 @@ function EmployerList(props) {
   );
 }
 
-function HelpTextForRejection(props) {
-    return(
-        <HjelpetekstAuto  >
-            Innholdet vil vises når brukeren klikker på knappen.
 
-        </HjelpetekstAuto>
-    )
-}
 
 function EmployerSummary(props) {
     return (
@@ -186,9 +198,9 @@ console.log(props.periode.startDateYearMonth);
     var moment = require('moment');
     moment.locale('nb');
     return(
-        <li>
+        <ul>
             Periode:  {(props.periode.startDateYearMonth === props.periode.endDateYearMonth) ? moment(props.periode.startDateYearMonth, 'YYYY-MM').format('MMMM YYYY') :  moment(props.periode.startDateYearMonth, 'YYYY-MM').format('MMMM YYYY') +" til " +moment(props.periode.endDateYearMonth, 'YYYY-MM').format('MMMM YYYY') }
-        </li>
+        </ul>
     )
 
 }
@@ -223,9 +235,9 @@ function Employer(props) {
 
 function Income(props) {
   return (
-      <li>
+      <ul>
             {props.income.verdikode + ": " + props.income.income}
-      </li>
+      </ul>
   )
 }
 function AllYears(props) {
