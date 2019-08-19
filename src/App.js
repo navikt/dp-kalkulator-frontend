bafb02d4e2fbebebc045285d1f98650b8aaebfa2
Knut Magne Riise <kriise@Knuts-MacBook-Pro.local>
2019-08-19 11:39:55 +0200

    started feedback for errors

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import personIncomeService from './services/PersonIncome'
 const App = () => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("")
   const [doesPersonQualify, setDoesPersonQualify] = useState(false)
   const [periodeAntalluker, setPeriodeAntallUker] = useState(0)
   const [ukesats, setUkesats] = useState(0.0)
@@ -27,45 +28,60 @@ const App = () => {
   useEffect(() => {
     personIncomeService.get()
       .then(personIncomeInformation => {
-        if (personIncomeInformation.ok) {
-          return personIncomeInformation.json()
+        let contentType = personIncomeInformation.headers.get("content-type")
+        if(contentType && contentType.includes("application/json"))
+        {
+          let json = personIncomeInformation.json()
+          if (personIncomeInformation.ok) {
+            setDoesPersonQualify(json.oppfyllerMinstekrav)
+            setPeriodeAntallUker(json.periodeAntalluker)
+            setUkesats(json.ukeSats)
+            setTotalIncome36(json.totalIncome36)
+            setTotalIncome12(json.totalIncome12)
+            setMonthsIncomeInformation(json.monthsIncomeInformation)
+            setEmployerSummaries(json.employerSummaries)
+            setPeriodIncome(json.periodIncome)
+          }
+          else {
+            let errorResponse = json.toJSON()
+            console.log("json = " + errorResponse.status)
+            console.log(json)
+            console.log(errorResponse)
+            setErrorMessage(errorResponse.title)
+            console.log("errormessage = " + errorResponse)
+            setError(true)
+          }
+
+          setLoading(false);
         }
-        throw Error(personIncomeInformation.statusText);
-      })
-      .then(json => {
-        setLoading(false);
-        setDoesPersonQualify(json.oppfyllerMinstekrav);
-        setPeriodeAntallUker(json.periodeAntalluker)
-        setUkesats(json.ukeSats)
-        setTotalIncome36(json.totalIncome36)
-        setTotalIncome12(json.totalIncome12)
-        setMonthsIncomeInformation(json.monthsIncomeInformation)
-        setEmployerSummaries(json.employerSummaries)
-        setPeriodIncome(json.periodIncome)
+        else {
+          throw Error("En feil uten feilmelding oppstod");
+        }
       })
       .catch(e => {
-        console.log(e)
         setLoading(false)
         setError(true)
       })
   }, [])
 
-  if (loading) { return (<LoadingMessage />); }
-  if (error) { return (<ErrorMessage />); }
-
-  if (monthsIncomeInformation.length === 0) {
-    return (<NoIncome doesPersonQualify={doesPersonQualify} />);
+  let feedback;
+  if (loading) { feedback = (<LoadingMessage />); }
+  else if (error) {
+    feedback = <ErrorMessage />
   }
+  else {
+    feedback =
+        <>
+        <InntektFiltrering/>
+        <QualifiedMessage doesPersonQualify={doesPersonQualify} ukeSats={ukesats} periodeAntalluker={periodeAntalluker} />
+        </>;
+  }
+
 
   return (
     <div className="App">
-      <Header loading={false} />
-      <InntektFiltrering />
-      <QualifiedMessage doesPersonQualify={doesPersonQualify} ukeSats={ukesats} periodeAntalluker={periodeAntalluker} />
-      <IncomeSummary totalIncome36={totalIncome36} totalIncome12={totalIncome12} />
-      <IncomeInPeriodList periodIncome={periodIncome} />
-      <RapporteringInfo />
-      <InformationSummary employerSummaries={employerSummaries} monthsIncomeInformation={monthsIncomeInformation} />
+      <Header loading={loading} />
+      { feedback }
     </div>
   );
 }
--- src/Components/information/ErrorMessage.js
+++ src/Components/information/ErrorMessage.js
@@ -1,14 +1,10 @@
 import React from 'react';
 import PanelBase from 'nav-frontend-paneler';
-import Header from '../Header';
 
 export default function ErrorMessage() {
   return (
-    <div className="App">
-      <Header loading={false} />
-      <PanelBase border>
+      <PanelBase className="Error" border>
         Vi opplevde en feil når vi prøvde å laste inn din data. Hvis feilen vedvarer kontakt nav.no.
       </PanelBase>
-    </div>
   );
 }
