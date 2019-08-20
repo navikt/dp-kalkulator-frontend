a0570289d94e096b01fc2d9857bb4820971e347b
Andreas Bergh <andreas.nygard.bergh@nav.no>
2019-08-20 08:47:36 +0200

    Using backticks instead of concatenation

--- src/Components/information/ErrorMessage.js
+++ src/Components/information/ErrorMessage.js
@@ -3,16 +3,17 @@ import PanelBase from 'nav-frontend-paneler';
 
 export default function ErrorMessage({ error }) {
   const parsedText = () => {
-    if (!error.data) {
-      return (error.status + " " + error.statusText)
+    if (error.data) {
+      return (`${error.status} ${error.data.title}`)
     }
     else {
-      return (error.status + " " + error.data.title)
+      return (`${error.status} ${error.statusText}`)
     }
   }
+  
   return (
     <PanelBase className="Error" border>
-      {parsedText()}
+      { parsedText() }
     </PanelBase>
   );
 }
