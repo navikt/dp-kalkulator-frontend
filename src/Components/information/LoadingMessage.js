74d4733259cf1717922d5ad1b1951d38bcc600c3
Andreas Bergh <andreas.nygard.bergh@nav.no>
2019-08-27 12:07:56 +0200

    Fixed loading message

--- src/Components/information/LoadingMessage.js
+++ src/Components/information/LoadingMessage.js
@@ -1,13 +1,27 @@
 import React from 'react';
 import PanelBase from 'nav-frontend-paneler';
-import Header from '../Header';
+import NavFrontendSpinner from 'nav-frontend-spinner';
+import { Normaltekst } from 'nav-frontend-typografi';
 
 export default function LoadingMessage() {
+  const style = {
+    display: 'flex',
+    justifyContent: 'center',
+    lineHeight: '1.7em',
+    paddingBottom: '3px'
+  }
+
   return (
-      <PanelBase border>
+      <PanelBase border >
+        <div style={style}>
+          <NavFrontendSpinner />
+        </div>
+        <Normaltekst style={style}>
         Vi laster nå inn informasjonen vi har om dine inntekter som danner grunnlaget for vurderingen om dagpenger.
-        <br />
+        <br/>
         Vennligst vent opp til 2 minutter før du laster inn siden på nytt.
+        </Normaltekst>
+       
       </PanelBase>
   );
 }
