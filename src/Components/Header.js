00186ea95f58684aa4f42de3afec21a526da3a80
Andreas Bergh <andreas.nygard.bergh@nav.no>
2019-08-27 12:54:20 +0200

    Changed title and removed unused prop

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,12 +2,12 @@ import { Sidetittel } from 'nav-frontend-typografi';
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default function Header({ loading }) {
+export default function Header() {
   return (
     <div className="header">
       <img src="https://www.nav.no/_public/beta.nav.no/images/logo.png?_ts=1512923c9b0" alt="NAV-logo-rød-trans-bg-200.png" />
       <Sidetittel>
-        Dagpengeestimator
+        Estimering av dagpenger
       </Sidetittel>
       <br />
       <br />
