1afff9b3a904312916f573d873055ed50673ae95
Andreas Bergh <andreas.nygard.bergh@nav.no>
2019-08-27 12:08:22 +0200

    removed spinner from header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,6 +1,5 @@
 import { Sidetittel } from 'nav-frontend-typografi';
 import React from 'react';
-import NavFrontendSpinner from 'nav-frontend-spinner';
 import PropTypes from 'prop-types';
 
 export default function Header({ loading }) {
@@ -8,8 +7,7 @@ export default function Header({ loading }) {
     <div className="header">
       <img src="https://www.nav.no/_public/beta.nav.no/images/logo.png?_ts=1512923c9b0" alt="NAV-logo-rød-trans-bg-200.png" />
       <Sidetittel>
-        Grunnlag for dagpenger - A-ordningen
-        {loading ? <NavFrontendSpinner /> : <div />}
+        Dagpengeestimator
       </Sidetittel>
       <br />
       <br />
