d619aae67a4aaec6fb2e5acd58cb7cf594e2cb15
Knut Magne Riise <kriise@Knuts-MacBook-Pro.local>
2019-08-29 11:46:41 +0200

    ESLinted app.js

--- src/App.js
+++ src/App.js
@@ -4,77 +4,68 @@ import Header from './Components/Header';
 import QualifiedMessage from './Components/QualifiedMessage';
 import LoadingMessage from './Components/information/LoadingMessage';
 import ErrorMessage from './Components/information/ErrorMessage';
-import personIncomeService from './services/PersonIncome'
-import TilbakeTilInfoKnapp from './Components/TilbakeTilInfoKnapp'
+import personIncomeService from './services/PersonIncome';
+import TilbakeTilInfoKnapp from './Components/TilbakeTilInfoKnapp';
 
 
 const App = () => {
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const [errorObject, setErrorObject] = useState([])
-  const [doesPersonQualify, setDoesPersonQualify] = useState(false)
-  const [periodeAntalluker, setPeriodeAntallUker] = useState(0)
-  const [ukesats, setUkesats] = useState(0.0)
-
-
-
-
+  const [errorObject, setErrorObject] = useState([]);
+  const [doesPersonQualify, setDoesPersonQualify] = useState(false);
+  const [periodeAntalluker, setPeriodeAntallUker] = useState(0);
+  const [ukesats, setUkesats] = useState(0.0);
 
 
   useEffect(() => {
-    const urlAPI = '/api/inntekt/'
-    const urlMock = `${process.env.PUBLIC_URL}/mock/mockInnsyn.json`
-
-    const fetchData = (url) => {
-      personIncomeService.get(url)
-          .then(personIncomeInformation => {
-            console.log(personIncomeInformation)
-            setData(personIncomeInformation)
-            setLoading(false);
-          })
-          .catch(e => {
-            let error = e.response
-            setErrorObject({data: error.data, status: error.status, statusText: error.statusText})
-            setLoading(false)
-            setError(true)
-          })
-    }
+    const urlAPI = '/api/inntekt/';
+    const urlMock = `${process.env.PUBLIC_URL}/mock/mockInnsyn.json`;
 
     const setData = (json) => {
       setDoesPersonQualify(json.oppfyllerMinstekrav);
-      setPeriodeAntallUker(json.periodeAntalluker)
-      setUkesats(json.ukeSats)
-    }
+      setPeriodeAntallUker(json.periodeAntalluker);
+      setUkesats(json.ukeSats);
+    };
 
-    if (process.env.NODE_ENV === 'production')
-    {
+    const fetchData = (url) => {
+      personIncomeService.get(url)
+        .then((personIncomeInformation) => {
+          console.log(personIncomeInformation);
+          setData(personIncomeInformation);
+          setLoading(false);
+        })
+        .catch((e) => {
+          const retrievedError = e.response;
+          setErrorObject({ data: retrievedError.data, status: retrievedError.status, statusText: retrievedError.statusText });
+          setLoading(false);
+          setError(true);
+        });
+    };
+
+
+    if (process.env.NODE_ENV === 'production') {
       fetchData(urlAPI);
-    }
-    else {
+    } else {
       fetchData(urlMock);
     }
-
-  }, [])
+  }, []);
 
   let feedback;
-  if (loading) { feedback = (<LoadingMessage />); }
-  else if (error) {
-    feedback = <ErrorMessage error={errorObject}/>
-  }
-  else {
-    feedback =
-        <QualifiedMessage doesPersonQualify={doesPersonQualify} ukeSats={ukesats} periodeAntalluker={periodeAntalluker} />
+  if (loading) { feedback = (<LoadingMessage />); } else if (error) {
+    feedback = <ErrorMessage error={errorObject} />;
+  } else {
+    feedback = <QualifiedMessage doesPersonQualify={doesPersonQualify} ukeSats={ukesats} periodeAntalluker={periodeAntalluker} />;
   }
 
 
   return (
     <div className="App">
       <div className="row">
-          <Header className="maxWidth"/>
+        <Header className="maxWidth" />
       </div>
       <div className="row">
         <div className="col-xs-12">
-          <TilbakeTilInfoKnapp/>
+          <TilbakeTilInfoKnapp />
         </div>
       </div>
       <div className="row">
@@ -84,15 +75,13 @@ const App = () => {
       </div>
       <div className="row">
         <div className="col-xs-12">
-          <TilbakeTilInfoKnapp/>
+          <TilbakeTilInfoKnapp />
         </div>
       </div>
 
 
-
-
     </div>
   );
-}
+};
 
 export default App;
