32e190d79cb66d98c28c136d39fceeb06fe9368f
Knut Magne Riise <kriise@Knuts-MacBook-Pro.local>
2019-08-29 15:23:05 +0200

    #220 centered items and added padding/margins

--- src/Components/Consent.js
+++ src/Components/Consent.js
@@ -10,10 +10,22 @@ export default function Consent({consent, fetchData, toggle, feilMelding}) {
 
     }
     const minHeight = {
-        minHeight: '80px'
+        minHeight: '100px'
     }
     const fontSize = {
         fontSize: '1.3em'
+    }
+    const centered = {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
+    const margin = {
+        margin: '5px',
+    }
+
+    const padding = {
+        padding: '5px'
     }
 
     const isChecked = () => {
@@ -32,12 +44,12 @@ export default function Consent({consent, fetchData, toggle, feilMelding}) {
                 </div>
             </div>
             <div className={'row'}>
-                <div style={minHeight} className={'col-xs-12'}>
+                <div style={{...minHeight, ...centered, ...padding}} className={'col-xs-12'}>
                     <Checkbox style={{...fontSize}} onChange={toggle} checked={consent} label={'Jeg samtykker til at NAV innhenter lønnsopplysningene mine'} feil={isChecked()}/>
                 </div>
             </div>
             <div className={'row'}>
-                <div className={'col-xs-12'}>
+                <div className={'col-xs-12'} style={{...centered, ...margin}}>
                     <Hovedknapp onClick={fetchData}>Estimer dagpenger</Hovedknapp>
                 </div>
             </div>
