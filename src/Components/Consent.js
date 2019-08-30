7dc656071045f5ee1724ca9c3fdee9cb34a1df8c
Andreas Bergh <andreas.nygard.bergh@nav.no>
2019-08-30 12:22:25 +0200

    Minor style changes

--- src/Components/Consent.js
+++ src/Components/Consent.js
@@ -1,10 +1,10 @@
 import React from "react";
 import Panel from "nav-frontend-paneler";
-import {Hovedknapp} from "nav-frontend-knapper";
-import {Checkbox} from "nav-frontend-skjema";
-import {Normaltekst} from "nav-frontend-typografi";
+import { Hovedknapp } from "nav-frontend-knapper";
+import { Checkbox } from "nav-frontend-skjema";
+import { Normaltekst } from "nav-frontend-typografi";
 
-export default function Consent({consent, fetchData, toggle, feilMelding}) {
+export default function Consent({ consent, fetchData, toggle, feilMelding }) {
     const panelStyle = {
         background: '#e0f5fb'
 
@@ -12,17 +12,29 @@ export default function Consent({consent, fetchData, toggle, feilMelding}) {
     const minHeight = {
         minHeight: '80px'
     }
+    const flex = {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center'
+    }
+    const maxWidth = {
+        maxWidth: '600px'
+    }
     const fontSize = {
         fontSize: '1.3em'
     }
 
+    const padding = {
+        padding: '10px'
+    }
+
     const isChecked = () => {
-        return(!consent && feilMelding ? {feilmelding: 'Du må samtykke for å kunne estimere'} : null)
+        return (!consent && feilMelding ? { feilmelding: 'Du må samtykke for å kunne estimere' } : null)
     }
 
     return (
-        <Panel style={panelStyle}>
-            <div className={'row'}>
+        <Panel style={{ ...panelStyle, ...maxWidth, ...flex }}>
+            <div className={'row'} style={padding}>
                 <div className={'col-xs-12'}>
                     <Normaltekst>
                         For at vi skal kunne estimere dagpengemulighetene dine, innhenter NAV data om inntektene dine fra skattevesenet.
@@ -31,17 +43,15 @@ export default function Consent({consent, fetchData, toggle, feilMelding}) {
                     </Normaltekst>
                 </div>
             </div>
-            <div className={'row'}>
-                <div style={minHeight} className={'col-xs-12'}>
-                    <Checkbox style={{...fontSize}} onChange={toggle} checked={consent} label={'Jeg samtykker til at NAV innhenter lønnsopplysningene mine'} feil={isChecked()}/>
-                </div>
+            <div className='row' style={{ ...padding, ...{ alignSelf: 'start' } }}>
+                <Checkbox style={{ ...fontSize }} onChange={toggle} checked={consent} label={'Jeg samtykker til at NAV innhenter lønnsopplysningene mine'} feil={isChecked()} />
             </div>
             <div className={'row'}>
                 <div className={'col-xs-12'}>
                     <Hovedknapp disabled={!consent} onClick={fetchData}>Estimer dagpenger</Hovedknapp>
                 </div>
             </div>
-        </Panel>
+        </Panel >
     );
 
 }
