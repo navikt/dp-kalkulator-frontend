718f2bcaf1183b51a9f760c97266ef5445a79efd
Knut Magne Riise <kriise@Knuts-MacBook-Pro.local>
2019-09-18 12:34:44 +0200

    removed static rendering that didnt work, tried rendering index instead

--- src/server.js
+++ src/server.js
@@ -1,17 +1,15 @@
 const express = require('express');
 const app = express();
 
-app.configure(function(){
-    app.set("view options", {layout: false})
-    app.use(express.static(__dirname));
-})
+//app.use(express.static(__dirname));
 
 app.get('/', function(req, res) {
     res.render('index.html');
+    //res.sendFile('index.html');
 });
 
 app.get('/health/is-alive', function(req, res) {
-    res.status(200).send("hei")
+    res.status(200).send("hei");
 });
 
 app.listen(443);
