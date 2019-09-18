0c338bc843a616f2cf328ea1a1a238bce0262822
Knut Magne Riise <kriise@Knuts-MacBook-Pro.local>
2019-09-18 13:39:39 +0200

    tried another way of solving the express pathing

--- public/server.js
+++ public/server.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.static(__dirname));
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'index.html'));
 });
 
 app.get('/health/is-alive', function(req, res) {
