bcf96e564f1b67480389f4cd5b72465275e8a535
Knut Magne Riise <kriise@Knuts-MacBook-Pro.local>
2019-09-24 12:07:35 +0200

    added context so that node can serve favicon

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-app.use(express.static(__dirname));
+app.use('/arbeid/dagpenger/kalkulator', express.static(__dirname));
 
 app.use(
     '/arbeid/dagpenger/kalkulator/static/js',
