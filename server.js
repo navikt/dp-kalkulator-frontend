3e054966c30925f138bc12f5ccb8057ef1c73e12
Knut Magne Riise <kriise@Knuts-MacBook-Pro.local>
2019-09-24 11:08:44 +0200

    added paths for scriptfiles and css in server.js

--- server.js
+++ server.js
@@ -3,6 +3,18 @@ const express = require('express');
 const app = express();
 
 app.use(express.static(__dirname));
+app.use(
+    '/static/js',
+    express.static(path.resolve(__dirname, 'static/js'))
+);
+app.use(
+    '/static/css',
+    express.static(path.resolve(__dirname, 'static/css'))
+);
+app.use(
+    '/static/media',
+    express.static(path.resolve(__dirname, 'static/media'))
+);
 
 app.get('/arbeid/dagpenger/kalkulator/', function(req, res) {
     res.sendFile(path.resolve(__dirname, 'index.html'));
