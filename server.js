3f806ea86ea84ef91134dc09fa3f6bc4fd04b4f1
Knut Magne Riise <kriise@Knuts-MacBook-Pro.local>
2019-09-24 12:03:05 +0200

    re-added necessary code

--- server.js
+++ server.js
@@ -4,6 +4,19 @@ const app = express();
 
 app.use(express.static(__dirname));
 
+app.use(
+    '/arbeid/dagpenger/kalkulator/static/js',
+    express.static(path.resolve(__dirname, 'static/js'))
+);
+app.use(
+    '/arbeid/dagpenger/kalkulator/static/css',
+    express.static(path.resolve(__dirname, 'static/css'))
+);
+app.use(
+    '/arbeid/dagpenger/kalkulator/static/media',
+    express.static(path.resolve(__dirname, 'static/media'))
+);
+
 app.get('/arbeid/dagpenger/kalkulator/', function(req, res) {
     res.sendFile(path.resolve(__dirname, 'index.html'));
 });
