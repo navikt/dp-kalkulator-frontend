08869c49359205fb84ef15fcc478d403b0851a7a
Per Odin Palmquist Andersen <perodinpalmquistandersen@Pers-MacBook-Pro.local>
2019-10-09 10:53:51 +0200

    Switched button html type upon language change for visual satisfaction.

--- src/Components/Kalkulator/sprak/sprakvelger.js
+++ src/Components/Kalkulator/sprak/sprakvelger.js
@@ -1,26 +1,43 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState, useRef, useEffect } from 'react'
 import { Knapp } from 'nav-frontend-knapper'
 import i18n from '../../../i18n'
 
+
+// TODO:
+// Fix so that buttons lose focus/hover?
+// design.nav.knapp has a button outline/background when active&focused we dont want
 const Sprakvelger = () => {
+    const [ enButton, setEnButton ]  = useState("standard")
+    const [ nbButton, setNbButton ] = useState("standard")
+    const unFocus = useRef(null)
+
+    useEffect(() => {
+        unFocus.current.focus()
+        console.log("current focus is: " + unFocus.current)
+    }, [enButton, nbButton]);
 
     const handleClickEN = () => {
+        setEnButton("hoved")
+        setNbButton("standard")
+        unFocus.current.focus()
         i18n.changeLanguage("en")
-        console.log("handleClickEN")
         }
 
     const handleClickNB = () => {
         i18n.changeLanguage("nb")
+        setEnButton("standard")
+        setNbButton("hoved")
+        unFocus.current.focus()
         }
+    
 
-    return <div>
-        <Knapp form="kompakt" onClick = {handleClickEN}>
+    return <div ref={unFocus}>
+        <Knapp form="kompakt" onClick = {handleClickEN} type={enButton} htmlType="button">
         <span>EN</span>
 
        </Knapp>
-        <Knapp form="kompakt" onClick = {handleClickNB}>
+        <Knapp form="kompakt" onClick = {handleClickNB} type={nbButton} htmlType="button">
         <span>NB</span>
-
         </Knapp>
     </div>
 } 
