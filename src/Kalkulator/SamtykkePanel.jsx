c047bad2587eec67e6abdd7bbcefd726ef83bd62
Petter Ruud <petter.ruud@nav.no>
2019-10-28 13:58:24 +0100

    Tekstendringer

--- src/Kalkulator/SamtykkePanel.jsx
+++ src/Kalkulator/SamtykkePanel.jsx
@@ -2,8 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Panel from 'nav-frontend-paneler';
 import { Hovedknapp } from 'nav-frontend-knapper';
-import { useTranslation } from 'react-i18next';
+import { useTranslation, Trans } from 'react-i18next';
 import { Normaltekst, Innholdstittel } from 'nav-frontend-typografi';
+import Lenke from 'nav-frontend-lenker';
+
 import Spacer from '../Components/Spacer';
 
 export const SamtykkePanel = ({ onClickCallback }) => {
@@ -12,7 +14,13 @@ export const SamtykkePanel = ({ onClickCallback }) => {
     <Panel>
       <div className="padding16">
         <Innholdstittel tag="h2">{t('SAMTYKKEPANEL.HENTER_INFO_FRA_SKATTEETAEN_TITTEL')}</Innholdstittel>
-        <Normaltekst>{t('SAMTYKKEPANEL.HENTER_INFO_FRA_SKATTEETAEN_INGRESS')}</Normaltekst>
+
+        <Normaltekst>
+          <Trans i18nKey="SAMTYKKEPANEL.HENTER_INFO_FRA_SKATTEETAEN_INGRESS">
+            <Lenke href="https://skatteetaten.no/mineinntekter" />
+          </Trans>
+        </Normaltekst>
+        <Normaltekst>{t('SAMTYKKEPANEL.OPPLYSNINGENE_SLETTES')}</Normaltekst>
         <Spacer twentyPx />
         <div className="flex center">
           <Hovedknapp onClick={onClickCallback}>{t('KNAPP.FORTSETT')}</Hovedknapp>
