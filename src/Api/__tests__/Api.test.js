c0ac74899319fd23d8a30194f8ba5570ebc34d9e
Petter Ruud <petter.ruud@nav.no>
2019-11-22 10:02:01 +0100

    Fikser og leager flere tester

--- src/Api/__tests__/Api.test.js
+++ src/Api/__tests__/Api.test.js
@@ -1,13 +1,47 @@
+import axios from 'axios';
+import MockAdapter from 'axios-mock-adapter';
+import { expect } from 'chai';
 import { getLoginUrl } from '../Config';
+import mockInnsyn from '../../../public/__mocks__/mockInnsyn.json';
+
+const mock = new MockAdapter(axios);
 
 describe('Api', () => {
   it('Skal gi tilbake riktig url T6 og T1', () => {
     const url = getLoginUrl('https://t6.no');
-    expect(url).toEqual('https://loginservice.nav.no/login?level=Level3');
+    expect(url).to.equal('https://loginservice.nav.no/login?level=Level3');
   });
 
   it('Skal gi tilbake riktig url Q', () => {
     const url = getLoginUrl('https://www-q.no');
-    expect(url).toEqual('https://loginservice-q.nav.no/login?level=Level3');
+    expect(url).to.equal('https://loginservice-q.nav.no/login?level=Level3');
+  });
+
+  it('Hente behov', async () => {
+    mock.onGet(`/behov`).reply(200, mockInnsyn);
+    try {
+      const response = await axios.get('/behov', {});
+      expect(response.data).to.eql(mockInnsyn);
+    } catch (error) {
+      throw new Error(error);
+    }
+  });
+
+  it('Hente behov timer ut', async () => {
+    mock.onGet(`/behov`).timeout();
+    try {
+      await axios.get('/behov', {});
+    } catch (error) {
+      expect(error.code).to.eql('ECONNABORTED');
+    }
+  });
+
+  it('Hente behov skal gi 401 når du ikke er logget inn', async () => {
+    mock.onGet(`/behov`).reply(401, {});
+    try {
+      await axios.get('/behov', {});
+    } catch (error) {
+      expect(error.response.status).to.eql(401);
+    }
   });
 });
--- src/Kalkulator/Kalkulator.jsx
+++ src/Kalkulator/Kalkulator.jsx
@@ -4,7 +4,7 @@ import { getBehov } from '../Api';
 import QualifiedMessage from './QualifiedMessage';
 import tracking from '../lib/tracking';
 
-const Kalkulator = () => {
+export const Kalkulator = () => {
   const [isLoading, setLoading] = useState(true);
   const [isOppfyllerInntekstkrav, setOppfyllerInntekstkrav] = useState(false);
   const [periodeAntallUker, setPeriodeAntallUker] = useState(0);
@@ -20,20 +20,21 @@ const Kalkulator = () => {
           setPeriodeAntallUker(uker);
           setUkesats(sats);
           setLoading(false);
-          tracking.logEvent('RESULTAT', {
-            isOppfyllerInntekstkrav,
-          });
         }
       } catch (error) {
         throw new Error(error);
       }
     };
     fetchData();
-  }, [isOppfyllerInntekstkrav]);
+  }, []);
 
   if (isLoading) {
     return <LoadingMessage type="XL" />;
   }
+
+  tracking.logEvent('RESULTAT', {
+    isOppfyllerInntekstkrav,
+  });
 
   return <QualifiedMessage isOppfyllerInntekstkrav={isOppfyllerInntekstkrav} ukesats={ukesats} periodeAntallUker={periodeAntallUker} />;
 };
