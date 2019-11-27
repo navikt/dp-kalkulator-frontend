334aa2814eed275defadd84d4b969467f8831416
Petter Ruud <petter.ruud@nav.no>
2019-11-27 09:41:32 +0100

    Tekstendringer

--- src/Kalkulator/__tests__/Kalkulator.test.jsx
+++ src/Kalkulator/__tests__/Kalkulator.test.jsx
@@ -1,27 +1,13 @@
 import React from 'react';
-import MockAdapter from 'axios-mock-adapter';
-import { shallow, mount } from 'enzyme';
+import { shallow } from 'enzyme';
 import { expect } from 'chai';
-import { instance } from '../../Api';
 import { Kalkulator } from '../Kalkulator';
 import LoadingMessage from '../../Components/LoadingMessage';
-import mockInnsyn from '../../../public/__mocks__/mockInnsyn.json';
-
-const mock = new MockAdapter(instance);
 
 describe('Kalkulator', () => {
   it('Skal vise laster siden', () => {
     const wrapper = shallow(<Kalkulator />);
     const loadingMessage = wrapper.find(LoadingMessage);
-    expect(loadingMessage).to.have.length(1);
-  });
-
-  it('Skal vise kalkulator med resultat', async () => {
-    mock.onGet('/behov').reply(200, mockInnsyn);
-
-    const wrapper = await mount(<Kalkulator />);
-    const loadingMessage = wrapper.find(LoadingMessage);
-    console.log(wrapper.debug());
     expect(loadingMessage).to.have.length(1);
   });
 });
--- src/lib/tracking.js
+++ src/lib/tracking.js
@@ -2,9 +2,9 @@ import amplitude from 'amplitude-js';
 
 const getApiKey = () => {
   if (window.location.hostname.includes('www.nav.no')) {
-    return 'edf391bf01b758a289ef5e7cb297f77a';
+    return 'edf391bf01b758a289ef5e7cb297f77a'; // prod
   }
-  return '24eb6d83cfc9883c04c4eaec61251bf4';
+  return '24eb6d83cfc9883c04c4eaec61251bf4'; // dev
 };
 
 const tracking = amplitude.getInstance();
