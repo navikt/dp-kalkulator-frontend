d8a3d2896acad39f5b6afb265db60be73fc63ffa
Daniel Winsvold <danielwinsvold@gmail.com>
2020-05-14 12:36:49 +0200

    forenkler lessoppsett, tar ibruk craco
    tar ibruk craco og craco-less for å slippe ekstra less-bygge-script, less filer kan nå importeres direkte i komponenter

--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,7 @@ import Spinner from 'nav-frontend-spinner';
 import { I18nextProvider } from 'react-i18next';
 import App from './App/App';
 import i18n from './lib/i18n';
-import './index.css';
+import './index.less';
 
 ReactDOM.render(
   <I18nextProvider i18n={i18n}>
