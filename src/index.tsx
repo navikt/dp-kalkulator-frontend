b279e9e053cb7148a3c5920868b12583c0a09fe1
Daniel Winsvold <danielwinsvold@gmail.com>
2020-10-01 15:11:57 +0200

    renamer index.jsx -> index.tsx så vi er igang med typescript. react-scripts genererte en default tsconfig.json

---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom";
+import Spinner from "nav-frontend-spinner";
+import { I18nextProvider } from "react-i18next";
+import App from "./App/App";
+import i18n from "./lib/i18n";
+import "./index.less";
+
+ReactDOM.render(
+  <I18nextProvider i18n={i18n}>
+    <Suspense fallback={<Spinner />}>
+      <App />
+    </Suspense>
+  </I18nextProvider>,
+  document.getElementById("root")
+);
