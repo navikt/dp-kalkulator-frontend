6a1bdfdc2fff5bb045a694e80276042c147b80cd
Morten Jansrud <morten.jansrud@nav.no>
2021-03-02 13:42:22 +0100

    Legg til client-side render

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,18 @@ import Spinner from "nav-frontend-spinner";
 import { I18nextProvider } from "react-i18next";
 import App from "./App/App";
 import i18n from "./lib/i18n";
+import { injectDecoratorClientSide } from "@navikt/nav-dekoratoren-moduler";
 import "./index.less";
+
+if (process.env.NODE_ENV === "development") {
+  injectDecoratorClientSide({
+    env: "prod",
+    breadcrumbs: [
+      { title: "Arbeidssøker eller permittert", url: "https://www.nav.no/arbeid/no/" },
+      { title: "Dagpengekalkulator", url: "https://www.nav.no/arbeid/dagpenger/kalkulator/" },
+    ],
+  });
+}
 
 ReactDOM.render(
   <I18nextProvider i18n={i18n}>
