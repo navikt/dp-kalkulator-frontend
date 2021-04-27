f2a2ec839d60d15b179bb8bc2d6aebda8fec78ad
Kriise <knut.magne.riise@nav.no>
2021-04-27 14:35:01 +0200

    logger lenkeklikk endret variabel til å sjekke om production fremfor notdevelopment for lesbarhet navikt/dagpenger#758

--- src/client.js
+++ src/client.js
@@ -1,8 +1,8 @@
 import sanityClient from "@sanity/client";
-import { isDevelopment } from "./utils/environment";
+import { isProduction } from "./utils/environment";
 
 export default sanityClient({
-  projectId: "rt6o382n", // find this at manage.sanity.io or in your sanity.json
-  dataset: "development", // this is from those question during 'sanity init'
-  useCdn: !isDevelopment(),
+  projectId: "rt6o382n",
+  dataset: "production",
+  useCdn: isProduction(),
 });
--- src/utils/BlockContent.tsx
+++ src/utils/BlockContent.tsx
@@ -4,6 +4,7 @@ import SanityBlockContent from "@sanity/block-content-to-react";
 import { VariabelSerializer } from "./VariabelSerializer";
 import { BlockRenderer } from "./BlockSerializer";
 import Lenke from "nav-frontend-lenker";
+import { logLenkeKlikk } from "../lib/tracking";
 
 const serializers = {
   types: {
@@ -11,7 +12,11 @@ const serializers = {
     block: BlockRenderer,
   },
   marks: {
-    link: (props: any) => <Lenke href={props.mark.href}>{props.children}</Lenke>,
+    link: (props: any) => (
+      <Lenke href={props.mark.href} onClick={() => logLenkeKlikk(props.mark.href)}>
+        {props.children}
+      </Lenke>
+    ),
   },
 };
 
