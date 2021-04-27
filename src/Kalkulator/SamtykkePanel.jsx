c8f4f897707be2993bbdd8644a760a043b8e0b3a
Kriise <knut.magne.riise@nav.no>
2021-04-27 09:49:49 +0200

    fjerner unused imports navikt/dagpenger#758

--- src/Kalkulator/SamtykkePanel.jsx
+++ src/Kalkulator/SamtykkePanel.jsx
@@ -2,20 +2,12 @@ import React from "react";
 import PropTypes from "prop-types";
 import Panel from "nav-frontend-paneler";
 import { Hovedknapp } from "nav-frontend-knapper";
-import tracking from "../lib/tracking";
 import Spacer from "../Components/Spacer";
-import { LENKER } from "../lib/constants";
 import { useTextContext } from "../utils/TextProvider";
 import BlockContent from "../utils/BlockContent";
 
 export const SamtykkePanel = ({ onClickCallback }) => {
   const text = useTextContext();
-
-  const handleClickMineInntekter = (event) => {
-    event.preventDefault();
-    tracking.logEvent("TIL_SKATTEETATEN");
-    window.location.assign(LENKER.MINE_INNTEKTER_URL);
-  };
 
   return (
     <Panel>
