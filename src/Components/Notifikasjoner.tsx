1d70c2ce4214a1f316784e37cb084417f6d3933d
Kriise <knut.magne.riise@nav.no>
2021-04-28 14:49:08 +0200

    lagt til støtte for notifikasjoner fra sanity navikt/dagpenger#758

---
+++ src/Components/Notifikasjoner.tsx
@@ -0,0 +1,37 @@
+import * as React from "react";
+import { AlertStripeInfo } from "nav-frontend-alertstriper";
+import { Element } from "nav-frontend-typografi";
+import BlockContent from "../utils/BlockContent";
+import { useTextContext } from "../utils/TextProvider";
+
+export interface Notifikasjon {
+  title?: string;
+  innhold?: typeof BlockContent[];
+}
+
+interface Props {
+  notifikasjoner?: Notifikasjon[];
+}
+
+function Notifikasjoner() {
+  const { notifikasjoner } = useTextContext();
+
+  if (!notifikasjoner?.length) {
+    return null;
+  }
+
+  console.log(notifikasjoner);
+
+  return (
+    <div>
+      {notifikasjoner.map((notifikasjon, i) => (
+        <AlertStripeInfo key={i}>
+          <Element>{notifikasjon.title}</Element>
+          {notifikasjon.innhold && <BlockContent blocks={notifikasjon.innhold} />}
+        </AlertStripeInfo>
+      ))}
+    </div>
+  );
+}
+
+export default Notifikasjoner;
