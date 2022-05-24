6dd855612541843548e58aebf5398502b63fc496
Knut Riise <kmriise@users.noreply.github.com>
2022-05-24 16:01:33 +0200

    slå på kalkis igjen (#432)
    * slå på kalkis igjen etter fiks av api token-validering

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { init } from "@sentry/browser";
 import Header from "../Components/Header";
 import BackButton from "../Components/BackButton";
+import SamtykkePanel from "../Kalkulator/SamtykkePanel";
 import Spacer from "../Components/Spacer";
+import Kalkulator from "../Kalkulator/Kalkulator";
+import ErrorBoundary from "../Components/ErrorBoundary";
+import { instance } from "../Api";
 import "./App.less";
 import Notifikasjoner from "../Components/Notifikasjoner";
+import { logAktivitet } from "../lib/tracking";
 import PreviewBanner from "../Components/PreviewBanner";
 
 // sentry
@@ -16,6 +21,21 @@ init({
 });
 
 export const App = () => {
+  const [isSamtykke, setSamtykke] = useState(false);
+  const [errors, setError] = useState({ hasError: false, status: null, statusText: null });
+  // axios apply interceptor on response
+  instance.interceptors.response.use(
+    (response) => response,
+    (error) => setError({ hasError: true, ...error })
+  );
+
+  const handleSetSamtykke = () => {
+    logAktivitet({ aktivitet: "Bruker trykker på fortsettknapp for å starte dagpengeberegning (ukjent om innlogget eller ikke)" });
+    setSamtykke(true);
+  };
+
+  const hasSamtykke = window.location.search === "?samtykke=true";
+
   return (
     <div className="App typo-normal">
       <PreviewBanner />
@@ -23,7 +43,9 @@ export const App = () => {
         <Header />
         <div className="content">
           <Notifikasjoner />
-
+          <Spacer twentyPx />
+          <ErrorBoundary apiErrors={errors}>{isSamtykke || hasSamtykke ? <Kalkulator /> : <SamtykkePanel onClickCallback={handleSetSamtykke} />}</ErrorBoundary>
+          <Spacer twentyPx />
           <BackButton />
           <Spacer twentyPx />
         </div>
