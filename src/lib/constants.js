52082d9684e36574fdc343ab19d4ba840c994bb0
Hanne Eilertsen Olsen <hanne.eilertsen.olsen@nav.no>
2022-07-01 14:52:33 +0200

    Oppdaterer URL til faktasidene

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,6 +1,6 @@
 export const LENKER = {
   MINE_INNTEKTER_URL: "https://skatteetaten.no/mineinntekter",
-  DAGPENGER_FAKTASIDE_URL: "https://nav.no/arbeid/no",
+  DAGPENGER_FAKTASIDE_URL: "https://nav.no/arbeid/arbeidsledig-permittert",
   DAGPENGER_SØKNAD_URL: "https://www.nav.no/soknader/nb/person/arbeid/dagpenger",
 };
 
