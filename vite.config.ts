3d86b6b8802a511160dbecb904ae96e419dd67c9
Elisabeth Kongshavn <elisabeth_kongshavn@hotmail.com>
2026-09-01 13:49:12 +0200

    react router 8 dev server fiks med base

--- vite.config.ts
+++ vite.config.ts
@@ -3,20 +3,27 @@ import path from "path";
 import { defineConfig } from "vite";
 import devtoolsJson from "vite-plugin-devtools-json";
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   base:
-    process.env.NODE_ENV === "production"
+    command === "build"
       ? "https://cdn.nav.no/teamdagpenger/dp-kalkulator-frontend/client/"
-      : "/dagpenger/kalkulator",
+      : "/dagpenger/",
+
   plugins: [reactRouter(), devtoolsJson()],
+
+  server: {
+    open: "/dagpenger/kalkulator"
+  },
+
   build: {
     manifest: true,
     sourcemap: process.env.NODE_ENV !== "production"
   },
+
   resolve: {
     alias: {
       "~": path.resolve(import.meta.dirname, "./app")
     },
     tsconfigPaths: true
   }
-});
+}));
